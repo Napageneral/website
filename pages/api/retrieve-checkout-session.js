4ce876921eccb41b30e74044f941cfd0ad7cd011
Tyler Brandt <tyler@tylers-mbp-2.lan>
2024-09-13 14:24:12 -0500

    install stripe, add hostname

--- pages/api/retrieve-checkout-session.js
+++ pages/api/retrieve-checkout-session.js
@@ -3,7 +3,7 @@ export default async (req, res) => {
     const { session_id } = req.query;
   
     try {
-      const response = await fetch(`https://yourbackend.com/checkout-session?session_id=${session_id}`);
+      const response = await fetch(`https://chatstats.ai/checkout-session?session_id=${session_id}`);
       const data = await response.json();
       res.status(200).json(data);
     } catch (err) {
